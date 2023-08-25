1bcfd637308e06d89e8b84e3d494b26ba355972c
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-08-25 09:33:58 +0700

    commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const morgan = require('morgan')
+const path = require('path')
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//http loger
+app.use(morgan('combined'));
+
+//template engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log("PATH: ", path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
