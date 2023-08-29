d0c8d6b885887f0ee1e7f8abf95e30a72d3e0fd5
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-08-29 17:17:45 +0700

    video 18 - MVC routes & controllers - IMPORTANT

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const path = require('path')
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 //http loger
@@ -19,30 +21,15 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-console.log("PATH: ", path.join(__dirname, 'resources/views'))
+// rotues init
+route(app);
 
 app.use(express.urlencoded({
   extended: true
 })) //TH gửi thông tin qua dạng form html
 app.use(express.json()) // TH gửi từ code JS
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
 
-app.get('/news', (req, res) => {
-  // console.log(req.query)
-  res.render('news');
-});
-
-app.get("/search", (req, res) => {
-  res.render("search")
-});
-
-app.post("/search", (req, res) => {
-  console.log(req.body)
-  res.send("ok")
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
