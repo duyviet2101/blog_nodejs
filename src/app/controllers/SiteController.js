2b8534fd27e7d4faebfccb55207a9ef951907df1
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-08-30 11:28:00 +0700

    video 25 - Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,19 @@
 const Course = require('../models/Course');
+const {
+    mutipleMongooseToObject,
+    mongooseToObject,
+} = require('../../util/mongoose');
 
 class SiteController {
     // [GET]  /
-    async index(req, res) {
-        try {
-            const result = await Course.find({});
-            res.json(result);
-            // res.render('home');
-        } catch (error) {
-            res.status(400).json({ error: 'error!!!!!!' });
-        }
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
 
     // [GET] /search
