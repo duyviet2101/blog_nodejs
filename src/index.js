3a5b6330700fc14cce8b886fc98ad967c43f597c
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-09-17 10:56:11 +0700

    change port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const methodOverride = require('method-override')
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const route = require('./routes');
 const db = require('./config/db');
